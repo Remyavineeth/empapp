b9c331583cbd586bdc9378157964f10ab4978578
Remyavineeth <dhruvhome@vineeths-MacBook-Pro.local>
2021-02-25 15:52:13 -0600

    Log4J

--- src/main/java/com/archis/empapp/controller/EmpController.java
+++ src/main/java/com/archis/empapp/controller/EmpController.java
@@ -9,17 +9,17 @@ import com.archis.empapp.dto.Employee;
 @RestController
 @RequestMapping("/emp")
 public class EmpController {
-    final static Log log = LogFactory.getLog(EmpController.class);
+    final static Log logger = LogFactory.getLog(EmpController.class);
     @GetMapping(value = "/{id}")
     public String getName(@PathVariable("id") Long id){
 
-        log.debug("Employee Id from request : " + id);
+        logger.debug("Employee Id from request : " + id);
         return "myName";
     }
 
     @PostMapping
     public String create(@RequestBody Employee employee) {
-        log.debug("Employee name from request : " + employee.getName());
+        logger.debug("Employee name from request : " + employee.getName());
 
         return employee.getLocation();
     }
